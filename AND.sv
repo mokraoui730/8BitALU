ad65b07121eb8e752e944ecc43218ed1b7318918
MylanBui2 <bum@oregonstate.edu>
2026-05-28 15:03:11 -0700

    Update AND.sv
    built and operation module

--- AND.sv
+++ AND.sv
@@ -1,7 +1,14 @@
-module arithmetic( 
-  input logic [6:0] A, 
-  input logic [6:0] B,
+module AND_op( 
+  input logic [7:0] A, 
+  input logic [7:0] B,
   input logic opcode, 
   output result
 )
 // AND function //
+  always_comb begin
+    if (opcode)
+      result = A & B;
+    else
+      result = 8'b0;
+  end
+endmodule
