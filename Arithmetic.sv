1733b32b3841cd482debffa45d2aa53febf212f6
mokraoui730 <mokraoui@oregonstate.edu>
2026-05-28 11:50:08 -0700

    Update Arithmetic.sv
    Added inputs and outputs

--- Arithmetic.sv
+++ Arithmetic.sv
@@ -1,2 +1,10 @@
+module arithmetic( 
+  input logic [6:0] A, 
+  input logic [6:0] B,
+  input logic opcode, 
+  output overflow_flag, 
+  output result
+)
+  
 // Addition Function //
 // Subtraction Function// 
