0f8025d020a54016fe627013c86a9d7480ad07df
MylanBui2 <bum@oregonstate.edu>
2026-05-28 15:07:12 -0700

    Update XOR.sv

--- XOR.sv
+++ XOR.sv
@@ -1,1 +1,16 @@
 // XOR function //
+module XOR_op( 
+  input logic [7:0] A, 
+  input logic [7:0] B,
+  input logic opcode, 
+  output result
+)
+  // OR module
+  // A | B
+  always_comb begin
+    if (opcode)
+      result = A ^ B;
+    else
+      result = 8'b0;
+  end
+endmodule
