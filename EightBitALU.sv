cbefe9aaf01ffbe951399e6d67a1146f69204976
mokraoui730 <mokraoui@oregonstate.edu>
2026-05-28 10:50:04 -0700

    Update EightBitALU.sv
    Added enable_n and reset_n

--- EightBitALU.sv
+++ EightBitALU.sv
@@ -2,6 +2,8 @@ module EightBitALU(
 input [7:0] A,  // 8 bit input A
 input [7:0] B,  //8 bit input B
 input [2:0] opcode, 
+input	enable_n, 
+input reset_n,
 output [7:0] result, 
 output overflow_flag, 
 output zero_flag
