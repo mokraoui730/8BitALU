fd62d4ad96694b3942481d879ba7fab32fe917a3
mokraoui730 <mokraoui@oregonstate.edu>
2026-05-28 10:44:29 -0700

    Update EightBitALU.sv
    Declared Variables

--- EightBitALU.sv
+++ EightBitALU.sv
@@ -1,3 +1,12 @@
+module EightBitALU(
+input [7:0] A,  // 8 bit input A
+input [7:0] B,  //8 bit input B
+input [2:0] opcode, 
+output [7:0] result, 
+output overflow_flag, 
+output zero_flag
+)
+
 // Just A //
 
 // Just B //
