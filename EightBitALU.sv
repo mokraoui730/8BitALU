f8cc6b7567bbd3377cf5d0b3b8a71f783a24fcf6
Ava <ms.avakatz@gmail.com>
2026-06-03 12:14:55 -0700

    Update EightBitALU.sv

--- EightBitALU.sv
+++ EightBitALU.sv
@@ -1,12 +1,24 @@
 module EightBitALU(
-input [7:0] A,  // 8 bit input A
-input [7:0] B,  //8 bit input B
-input [2:0] opcode, 
-input	enable_n, 
-input reset_n,
-output [7:0] result, 
-output overflow_flag, 
+	input [7:0] A,  // 8 bit input A
+	input [7:0] B,  //8 bit input B
+	input [2:0] opcode, 
+	output [7:0] result, 
+	output overflow_flag, 
+	output zero_flag
 );
+
+
+Addition_Subtraction ADD(
+	.A(A),
+	.B(B),
+	/// check if need flag to disinct
+	.result(result),
+	.zero_flag(zero_flag)
+
+);
+
+
+
 
 typedef enum logic [2:0] {
         IDLE, AND, OR, XOR, APASS, BPASS, ADD, SUB
@@ -20,28 +32,28 @@ always_ff @(posedge reset) begin
 		state <= next_state;
 	end
 	
-always @(op) begin
+always_comb begin
 	// flags always set to zero before operation
 	overflow_flag = 1'b0;
+	zero_flag = 1'b0;
 	
 	// default state - idle or listening
 	next_state = IDLE; 
-	case (op)
-	
-		IDLE: begin
-			if(opcode == 3'b001) // 1
+	case (state)
+		IDLE: begin // check wether need idle state since it doesn't have flag
+			if(opcode == 3'b000) 
 				next_state = AND; 
-			else if (opcode == 3'b010) // 2
-				next_state = OR
-				else if(opcode == 3'b011) // 3
+			if (opcode == 3'b001)
+				next_state = OR;
+			if(opcode == 3'b010) 
 					next_state = XOR;
-			else if(opcode == 3'b100)  // 4
+			if(opcode == 3'b011) 
 					next_state = APASS;
-			else if(opcode == 3'b101) // 5
+			if(opcode == 3'b100) 
 					next_state = BPASS;
-			else if(opcode == 3'b110)  // 6
+			if(opcode == 3'b101) 
 					next_state = ADD;
-			else if(opcode == 3'b111) // 7
+			if(opcode == 3'b110) 
 					next_state = SUB;
 		end  
 		AND : next_state = IDLE; 
@@ -54,16 +66,44 @@ always @(op) begin
 		default: next_state = IDLE; 
 		
 	endcase
-// Just A //
+end
 
-// Just B //
 
-// Addition //
 
-//Subtraction//
 
-// AND //
+always_comb begin 
+	
+	case(state)
+	AND:
+	OR:
+	XOR:
+	APASS: result = A;
+	BPASS: result = B; 
+	ADD:
+	SUB:
+	endcase
 
-// OR //
+end
+	
+endmodule
 
-// XOR //
+// if we don't need to do a fsm design 
+always_comb begin
+	// flags always set to zero before operation
+	overflow_flag = 1'b0;
+	zero_flag = 1'b0;
+	
+	if (enable_n = 0) begin
+		case (opcode)
+			3'b000:
+			3'b001: 
+			3'b010: 
+			3'b011: result = A; 
+			3'b100: result = B; 
+			3'b101:
+			3'b110: 
+			default: 
+		endcase	
+	end
+
+end
