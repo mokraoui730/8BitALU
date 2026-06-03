82e6a5766ee3613feba9ea806a49cb5afd16b011
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-03 12:53:08 -0700

    Update Parser.sv
    update Parser to using result as output

--- Parser.sv
+++ Parser.sv
@@ -1,11 +1,12 @@
-module Parser #(parameter N = 6)(
-    input  logic [N-1:0] digit,
+module Parser (
+    input  logic [7:0] result,
     output logic [3:0]   ones,
-    output logic [3:0]   tens
+    output logic [3:0]   tens,
+    output logic [3:0] hundreth
 );
 
-    assign ones = digit % 10;
-    assign tens = digit / 10;
-    assign hundreth = digit / 100; 
+    assign ones =  result % 10;
+    assign tens = result / 10;
+    assign hundreth = result / 100; 
 
 endmodule
