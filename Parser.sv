0efbb5d5a690e3e22546b4b2888611a23324696e
MylanBui2 <bum@oregonstate.edu>
2026-06-03 13:16:59 -0700

    Update Parser.sv
    changed hundreds

--- Parser.sv
+++ Parser.sv
@@ -2,11 +2,11 @@ module Parser (
     input  logic [7:0] result,
     output logic [3:0]   ones,
     output logic [3:0]   tens,
-    output logic [3:0] hundreth
+    output logic [3:0] hundreds
 );
 
     assign ones =  result % 10;
     assign tens = result / 10;
-    assign hundreth = result / 100; 
+    assign hundreds = result / 100; 
 
 endmodule
