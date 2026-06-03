eb643f3b3d3b695496b15ffe94895f1da87f3089
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-03 12:54:45 -0700

    Update SevenSegmentDisplay.sv
    removed clock and additional segments

--- SevenSegmentDisplay.sv
+++ SevenSegmentDisplay.sv
@@ -4,23 +4,13 @@ module ALUDisplay(
     output logic [6:0]  Seg0,    // seconds ones  (rightmost)
     output logic [6:0]  Seg1,    // seconds tens
     output logic [6:0]  Seg2,    // minutes ones
-    output logic [6:0]  Seg3,    // minutes tens
-    output logic [6:0]  Seg4,    // hours ones
-    output logic [6:0]  Seg5     // hours tens    (leftmost)
 );
 
     logic [25:0] tickCount;
     logic [5:0]  seconds, minutes;
     logic [6:0]  hours;
 
-    Clock #(.TICKS_PER_SEC(50_000_000)) myClock (
-        .clk         (clk),
-        .reset_n     (reset_n),
-        .tickCount   (tickCount),
-        .secondsCount(seconds),
-        .minutesCount(minutes),
-        .hoursCount  (hours)
-    );
+   
 
     // Parsed digits
     logic [3:0] sec_ones,  sec_tens;
@@ -34,8 +24,6 @@ module ALUDisplay(
     SevenSegmentDecode seg0dec (.digit(sec_ones),  .segments(Seg0));
     SevenSegmentDecode seg1dec (.digit(sec_tens),  .segments(Seg1));
     SevenSegmentDecode seg2dec (.digit(min_ones),  .segments(Seg2));
-    SevenSegmentDecode seg3dec (.digit(min_tens),  .segments(Seg3));
-    SevenSegmentDecode seg4dec (.digit(hour_ones), .segments(Seg4));
-    SevenSegmentDecode seg5dec (.digit(hour_tens), .segments(Seg5));
+    
 
 endmodule
