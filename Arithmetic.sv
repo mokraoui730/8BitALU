96c3bf70a19d9ce68b6a525c073b22d3a2cdd0ec
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-03 14:27:15 -0700

    Update Arithmetic.sv

--- Arithmetic.sv
+++ Arithmetic.sv
@@ -1,7 +1,8 @@
 module arithmetic( 
   input logic [7:0] A, 
   input logic [7:0] B,
-  input logic sel_and, 
+  input logic sel_add, 
+  input logic sel_sub, 
   output logic overflow, 
   output logic [7:0] result
 );
@@ -11,19 +12,20 @@ module arithmetic(
   always_comb begin 
     if (sel_add) begin 
       //Addition: A + B 
-      full_result = (1'b0, A) + (1'b0, B);
-      result = full_result[7:0]; 
-      overflow = full_result[8]; 
+      assign full_result = A + B;
+      result = full_result[7:0];
+      overflow = full_result[8];
     end else if (sel_sub) begin
       //Subtraction: A - B 
-      full result = (1'b0, A) - (1'b0, B);
+      assign full_result = A - B;
       result = full_result[7:0]; 
       overflow = full_result[8]; 
     end else begin 
-      result = 8b'0;  //clear
+      result = 8'b0;  //clear
       overflow = 1'b0; 
     end 
   end 
+  
 endmodule
       
 
