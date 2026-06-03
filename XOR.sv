e23fb4868fa23632cde9f92cf84cdc9ff09797c1
Ava <ms.avakatz@gmail.com>
2026-06-03 12:45:02 -0700

    Update XOR.sv

--- XOR.sv
+++ XOR.sv
@@ -2,15 +2,11 @@
 module XOR_op( 
   input logic [7:0] A, 
   input logic [7:0] B,
-  input logic opcode, 
   output result
 )
   // OR module
   // A | B
-  always_comb begin
-    if (opcode)
-      result = A ^ B;
-    else
-      result = 8'b0;
-  end
+
+  result = A ^ B;
+  
 endmodule
