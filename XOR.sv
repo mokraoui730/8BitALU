37363c18ecef5887f1efbc30f9bd2519101d6854
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-03 14:29:09 -0700

    Update XOR.sv

--- XOR.sv
+++ XOR.sv
@@ -1,12 +1,12 @@
 // XOR function //
-module XOR_op( 
+module XOR( 
   input logic [7:0] A, 
   input logic [7:0] B,
   output result
-)
+);
   // OR module
   // A | B
 
-  result = A ^ B;
+  assign result = A ^ B;
   
 endmodule
