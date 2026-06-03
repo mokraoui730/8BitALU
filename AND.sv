1d84a35899f8e3dc2c2e2f199d6ebfa493b080a8
Ava <ms.avakatz@gmail.com>
2026-06-03 12:45:42 -0700

    Update AND.sv

--- AND.sv
+++ AND.sv
@@ -1,14 +1,8 @@
 module AND_op( 
   input logic [7:0] A, 
   input logic [7:0] B,
-  input logic opcode, 
   output logic [7:0] result
 )
 // AND function //
-  always_comb begin
-    if (opcode)
-      result = A & B;
-    else
-      result = 8'b0;
-  end
+result = A & B;
 endmodule
