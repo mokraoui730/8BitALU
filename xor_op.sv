877acc715ac3dcac508a979013f56f3eae5a7061
Ava <ms.avakatz@gmail.com>
2026-06-04 13:26:24 -0700

    Update xor_op.sv

--- xor_op.sv
+++ xor_op.sv
@@ -1,12 +1,9 @@
-// XOR function //
-module XOR( 
+module or_op( 
   input logic [7:0] A, 
   input logic [7:0] B,
-  output result
+  output logic [7:0] result 
 );
   // OR module
   // A | B
-
-  assign result = A ^ B;
-  
+  assign result = A | B;
 endmodule
