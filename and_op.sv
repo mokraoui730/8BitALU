66739ed32ca7f657f678104d4ec705054e7b5712
Ava <ms.avakatz@gmail.com>
2026-06-04 13:28:04 -0700

    Update and_op.sv

--- and_op.sv
+++ and_op.sv
@@ -1,8 +1,8 @@
-module AND( 
+module and_op( 
   input logic [7:0] A, 
   input logic [7:0] B,
   output logic [7:0] result
 );
 // AND function //
- assign result = A & B;
+	assign result = A & B;
 endmodule
