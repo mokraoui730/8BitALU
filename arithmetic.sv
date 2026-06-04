81e8b24c18d24b43607bd2b77547d7b5c5553518
Ava <ms.avakatz@gmail.com>
2026-06-04 13:25:52 -0700

    Update arithmetic.sv

--- arithmetic.sv
+++ arithmetic.sv
@@ -2,30 +2,25 @@ module arithmetic(
   input logic [7:0] A, 
   input logic [7:0] B,
   input logic sel_add, 
-  input logic sel_sub, 
   output logic overflow, 
   output logic [7:0] result
 );
 
-  logic [8:0] full_result; 
+  // logic [8:0] full_result; 
 
   always_comb begin 
     if (sel_add) begin 
       //Addition: A + B 
-      assign full_result = A + B;
-      result = full_result[7:0];
-      overflow = full_result[8];
-    end else if (sel_sub) begin
+      {overflow, result} = A + B;
+      //result = full_result[7:0];
+      //overflow = full_result[8];
+    end else begin
       //Subtraction: A - B 
-      assign full_result = A - B;
-      result = full_result[7:0]; 
-      overflow = full_result[8]; 
-    end else begin 
-      result = 8'b0;  //clear
-      overflow = 1'b0; 
-    end 
+      {overflow, result} = A - B;
+      //result = full_result[7:0]; 
+      //overflow = full_result[8]; 
   end 
+end
   
 endmodule
       
-
