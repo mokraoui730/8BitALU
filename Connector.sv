a5c0a14d144170990d0cd052dce36e5690cfb22d
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-06 13:39:52 -0700

    Update Connector.sv

--- Connector.sv
+++ Connector.sv
@@ -1,1 +1,3 @@
 // This file is a top-level wrapper that connects the ALU to the FPGA. This implementatin is a 3-phase input scheme that allows the user to input the opcode, input A and B. 
+module connector(
+  input logic 
