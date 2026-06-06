a9ca2f4a7563fb9d01f9ae4631509fe4166e8f48
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-06 13:40:08 -0700

    Update SevenSegmentDisplay.sv

--- SevenSegmentDisplay.sv
+++ SevenSegmentDisplay.sv
@@ -1,9 +1,9 @@
 module SevenSegmentDisplay(
     input  logic        reset_n,
-    input logic [7:0] result
+    input logic [7:0] result,
     output logic [6:0]  Seg0,    //  ones  (rightmost)
     output logic [6:0]  Seg1,    //  tens
-    output logic [6:0]  Seg2,    //  hundreds
+    output logic [6:0]  Seg2    //  hundreds
 );
    
     // Parsed digits
@@ -12,10 +12,10 @@ module SevenSegmentDisplay(
     logic [3:0] hundreds_parsed;
 
     Parser #(.N(7)) resultParser (
-        (.result(result)), 
-        (.ones(ones_parsed)), 
-        (.tens(tens_parsed)),
-        (.hundreds(hundreds_parsed))
+        .result(result), 
+        .ones(ones_parsed), 
+        .tens(tens_parsed),
+        .hundreds(hundreds_parsed)
     );
 
     SevenSegmentDecode seg0dec (.digit(ones_parsed),  .segments(Seg0));
