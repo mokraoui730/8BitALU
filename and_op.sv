8b63a9a30b24e1b8e8342bc08504c991c83ea18c
Ava <ms.avakatz@gmail.com>
2026-06-07 19:58:33 -0700

    Update and_op.sv

--- and_op.sv
+++ and_op.sv
@@ -1,8 +1,18 @@
+/*
+ * ECE 204 Eight Bit ALU Final Project
+ * and_op
+ *
+ * Author(s): Iman Mokraoui, Mylan Bui, Ava Katz
+ * Description: This file performs a bitwise AND operation between two eight-bit numbers. 
+ *
+ */
+
 module and_op( 
   input logic [7:0] A, 
   input logic [7:0] B,
   output logic [7:0] result
 );
+	
 // AND function //
 	assign result = A & B;
 endmodule
