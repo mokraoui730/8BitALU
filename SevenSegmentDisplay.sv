adc0b52a0eda3c989b7ab3336353319ad2d7f1e5
Ava <ms.avakatz@gmail.com>
2026-06-07 20:11:04 -0700

    Update SevenSegmentDisplay.sv

--- SevenSegmentDisplay.sv
+++ SevenSegmentDisplay.sv
@@ -1,3 +1,13 @@
+/*
+ * ECE 204 Eight Bit ALU Final Project
+ * SevenSegmentDisplay
+ *
+ * Author(s): Iman Mokraoui, Mylan Bui, Ava Katz
+ * Description: Displays the result of each operation on the FPGA hex display. 
+ * The ones digit is the rightmost display, and preceded by the tens and hundreds digit. 
+ *
+ */
+
 module SevenSegmentDisplay(
     input logic [7:0] result,
     output logic [6:0]  Seg0,    //  ones  (rightmost)
@@ -10,6 +20,7 @@ module SevenSegmentDisplay(
     logic [3:0] tens_parsed;
     logic [3:0] hundreds_parsed;
 
+    // wired to parser to seperate result into digits for diplay
     Parser resultParser(
         .result(result), 
         .ones(ones_parsed), 
@@ -17,6 +28,7 @@ module SevenSegmentDisplay(
         .hundreds(hundreds_parsed)
     );
 
+    // displays each digit on the fpga 
     SevenSegmentDecode seg0dec (.digit(ones_parsed),  .segments(Seg0));
     SevenSegmentDecode seg1dec (.digit(tens_parsed),  .segments(Seg1));
     SevenSegmentDecode seg2dec (.digit(hundreds_parsed),  .segments(Seg2));
