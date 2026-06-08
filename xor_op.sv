fbe7a22cadb088b8cb353d6a71d1afd1ba633850
mokraoui730 <mokraoui@oregonstate.edu>
2026-06-07 20:14:28 -0700

    Update xor_op.sv

--- xor_op.sv
+++ xor_op.sv
@@ -1,11 +1,19 @@
+/*
+ * ECE 204 Eight Bit ALU Final Project
+ * xor_op
+ *
+ * Author(s): Iman Mokraoui, Mylan Bui, Ava Katz
+ * Description: This file performs a bitwise XOR operation between two eight-bit numbers. 
+ * 
+ * 
+ *
+ */
 // XOR function //
 module xor_op( 
-  input logic [7:0] A, 
+  input logic [7:0] A,
   input logic [7:0] B,
   output logic [7:0] result 
 );
-  // OR module
-  // A | B
 
   assign result = A ^ B;
   
