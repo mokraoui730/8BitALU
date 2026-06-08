f624fd0096959d9a500ca5fee79e0a3c178c8e73
Ava <ms.avakatz@gmail.com>
2026-06-07 19:56:25 -0700

    Update Parser.sv

--- Parser.sv
+++ Parser.sv
@@ -1,3 +1,13 @@
+/*
+ * ECE 204 Eight Bit ALU Final Project
+ * Parser
+ *
+ * Author(s): Iman Mokraoui, Mylan Bui, Ava Katz
+ * Description: The parser seperates the digits of a number from the ones to 
+ * the hundreds digit. 
+ *
+ */
+
 module Parser (
     input  logic [7:0] result,
     output logic [3:0]   ones,
