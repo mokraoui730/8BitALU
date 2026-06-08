cba08398f3d3826b9a34a60e465d67feb2b1f20c
Ava <ms.avakatz@gmail.com>
2026-06-07 20:14:27 -0700

    Update or_op.sv

--- or_op.sv
+++ or_op.sv
@@ -1,9 +1,17 @@
+/*
+ * ECE 204 Eight Bit ALU Final Project
+ * or_op
+ *
+ * Author(s): Iman Mokraoui, Mylan Bui, Ava Katz
+ * Description: This file performs a bitwise OR operation between two eight-bit numbers. 
+ *
+ */
+
 module or_op( 
   input logic [7:0] A, 
   input logic [7:0] B,
   output logic [7:0] result 
 );
-  // OR module
-  // A | B
+  // OR operation
   assign result = A | B;
 endmodule
